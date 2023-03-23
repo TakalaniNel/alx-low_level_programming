fd9a7dcff25a3cb42c36208a1e12a501fa67a6f7
Takalani Neluheni <neluhenitakalani079@gmail.com>
2023-03-23 08:16:54 -0700

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,6 +3,7 @@
 * main - Test function for positive or negative
 * Return: 0
 */
+
 int main(void)
 {
 	int i;
@@ -10,5 +11,6 @@ int main(void)
 	i = 0;
 
 	positive_or_negative(i);
+
 	return (0);
 }
