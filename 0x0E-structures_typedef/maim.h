33284c8aba5277eb9121ff6ce6b398accdaab711
Takalani Neluheni <neluhenitakalani079@gmail.com>
2023-04-18 14:44:06 -0700

    Task 1

---
+++ 0x0E-structures_typedef/maim.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+
+#endif
